f3e9dc09db3646fe48a942417a7fe023df24c4e9
zypherson <taylorzypherson@gmail.com>
2025-12-06 11:35:33 -0500

    connected everythig in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useEffect } from "react";
-import { getAccessToken } from "./services/spotify";
+import { useState, useEffect } from "react";
+import SearchBar from "./Components/searchbar";
+import ArtistCard from "./Components/ArtistCard";
+import { getAccessToken, searchArtists } from "./services/spotify";
+import "./App.css";
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [artists, setArtists] = useState([]);
+
   useEffect(() => {
-    async function testSpotify() {
-      const token = await getAccessToken();
-      console.log("Spotify Token:", token);
+    async function fetchToken() {
+      const t = await getAccessToken();
+      setToken(t);
     }
-    testSpotify();
+    fetchToken();
   }, []);
 
-  return <h1>Music Discovery App</h1>;
+  async function handleSearch(query) {
+    if (!token) return;
+    const results = await searchArtists(query, token);
+    setArtists(results);
+  }
+
+  return (
+    <div>
+      <h1>🎵 Music Discovery App</h1>
+
+      <SearchBar onSearch={handleSearch} />
+
+      <div className="artist-grid">
+        {artists.map((a) => (
+          <ArtistCard key={a.id} artist={a} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
