390623d06c9d9bf732b9799d90c2b87784fcaade
zypherson <taylorzypherson@gmail.com>
2025-12-11 02:30:59 -0500

    setup react route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ArtistPage from "./pages/ArtistPage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/artist/:id" element={<ArtistPage />} />
+    </Routes>
+  </BrowserRouter>
+);
