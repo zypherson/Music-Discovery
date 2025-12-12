bbf661e925b76de1cf9e7f8ad26195694c1d6e27
zypherson <taylorzypherson@gmail.com>
2025-12-12 01:17:16 -0500

    made artist card clickable

--- src/Components/ArtistCard.jsx
+++ src/Components/ArtistCard.jsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router-dom";
+
 function ArtistCard({ artist }) {
-    const img =
-      artist.images && artist.images.length > 0
-        ? artist.images[0].url
-        : "https://via.placeholder.com/150";
-  
-    return (
+  const img =
+    artist.images && artist.images.length > 0
+      ? artist.images[0].url
+      : "https://via.placeholder.com/150";
+
+  return (
+    <Link to={`/artist/${artist.id}`} className="artist-link">
       <div className="artist-card">
         <img src={img} alt={artist.name} />
         <h3>{artist.name}</h3>
         <p>{artist.followers.total.toLocaleString()} followers</p>
       </div>
-    );
-  }
-  
-  export default ArtistCard;
-  
+    </Link>
+  );
+}
+
+export default ArtistCard;
