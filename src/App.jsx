890c066b88a27bcec6203d166e8df7efdb98e0bc
zypherson <taylorzypherson@gmail.com>
2026-01-02 02:45:35 -0500

    added event listener to app.jsx to render the favorite count

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import SearchBar from "./Components/searchbar";
 import ArtistCard from "./Components/ArtistCard";
 import { getAccessToken, searchArtists } from "./services/spotify";
 import "./App.css";
-
+import { getFavoritesCount } from "./utils/favorites";
 function App() {
   //app is running smoothly as expected 
   const [token, setToken] = useState(null);
   const [artists, setArtists] = useState([]);
-
+  const [favCount, setFavCount] = useState(() => getFavoritesCount());
   useEffect(() => {
     async function fetchToken() {
       const t = await getAccessToken();
       setToken(t);
     }
     fetchToken();
+    function handleFavoritesUpdate() {
+      setFavCount(getFavoritesCount());
+    }
+  
+    window.addEventListener("favoritesUpdated", handleFavoritesUpdate);
+  
+    return () => {
+      window.removeEventListener("favoritesUpdated", handleFavoritesUpdate);
+    };
   }, []);
 
   async function handleSearch(query) {
